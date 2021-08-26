f0081d5afe0ac4e4cc43999b111a30a81ae964eb
Scott Burack <itsmescottb123@gmail.com>
2021-08-26 17:07:56 -0400

    todos update and add

--- todo.js
+++ todo.js
@@ -17,6 +17,25 @@ const todoSchema = new mongoose.Schema({
     done: Boolean
 })
 
+todoSchema.statics.createDocument = function(params, callback) {
+  return Todo.create({ text: params.text, done: params.done },
+    function(err, todo) {
+        if (err) return handleError(err);
+        callback(todo)   
+    }
+  )
+}
+
+todoSchema.statics.updateDocument = function(id, params, callback) {
+    return Todo.findOneAndUpdate({ _id: id },
+        {
+            text: params.text,
+            done: params.done
+        }, function (err, todo){
+            callback(todo)
+        })
+}
+
 todoSchema.statics.all = function(callback) {
     return Todo.find({}, function(err, todos) {
         callback({ todos: todos })
@@ -25,4 +44,4 @@ todoSchema.statics.all = function(callback) {
 
 const Todo = mongoose.model('Todo', todoSchema);
 
-module.export = Todo;
+module.exports = Todo;
